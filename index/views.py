b8019dfca77f31b5c246d15e4ea3d351880625a5
mayank11196 <mayank11195@gmail.com>
2017-04-03 17:45:05 +0530

    Updated the index page to be the root page

--- index/views.py
+++ index/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-import datetime
 
 def index(request):
-	now = datetime.datetime.now()
-	return render(request, 'index.html', {'date':now})
+	
+	return render(request, 'index.html')
