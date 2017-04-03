53b2c27d7c5cc142128f005434a4a2c3cc73f4fe
mayank11196 <mayank11195@gmail.com>
2017-04-03 19:58:58 +0530

    Updated login method to take user data via forms and save in db

--- userlogin/urls.py
+++ userlogin/urls.py
@@ -2,6 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [	
-				url(r'^login/$', views.login),
-				url(r'^login/(?P<user_name>[a-z]{})/(?P<password>[a-z]{})/$', views.login),		
+				url(r'^login/$', views.login),	
 		]
--- userlogin/views.py
+++ userlogin/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .models import User
 
 
-def login(request, user_name=None, password=None):
+def login(request):
+	data = request.POST
+	user = User(f_name=data['first_name'], l_name=data['last_name'], user_name=data['username'])
+	user.save()
+	return render(request, 'userprofile.html', {'user_name':data['first_name']})
 	
-	return render(request, 'userlogin.html', {'user_name':user_name, 'password':password})
