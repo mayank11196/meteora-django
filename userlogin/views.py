bad0c6b02f59ac3aeaef4ab65f38099d39fcc3a5
mayank11196 <mayank11195@gmail.com>
2017-04-04 11:26:54 +0530

    Added userprofile template and updated views.login

--- userlogin/views.py
+++ userlogin/views.py
@@ -3,8 +3,10 @@ from .models import User
 
 
 def login(request):
-	data = request.POST
-	user = User(f_name=data['first_name'], l_name=data['last_name'], user_name=data['username'])
-	user.save()
-	return render(request, 'userprofile.html', {'user_name':data['first_name']})
-	
+	if 'first_name' in request.POST:
+		data = request.POST
+		user = User(f_name=data['first_name'], l_name=data['last_name'], user_name=data['username'])
+		user.save()
+		return render(request, 'userprofile.html', {'user_name':data['first_name']})
+	else:
+		return render(request, 'userprofile.html', {'user_name':'Stranger'})
