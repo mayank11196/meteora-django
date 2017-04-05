e32edceaf711377d2c606888cd82f2dad41042e7
mayank11196 <mayank11195@gmail.com>
2017-04-05 11:28:42 +0530

    Updated index app to display login form

--- index/forms.py
+++ index/forms.py
@@ -1,7 +1,13 @@
 from django import forms
 
-class UserForm(forms.Form):
-	first_name = forms.CharField()
-	last_name = forms.CharField()
-	username = forms.CharField()
-	#email_addr = forms.EmailField()
+class UserRegistrationForm(forms.Form):
+	first_name = forms.CharField(label='First Name')
+	last_name = forms.CharField(label='Last Name')
+	username = forms.CharField(label='Username')
+	email = forms.EmailField(label='Email')
+	password = forms.CharField(label='Password')
+
+
+class UserLoginForm(forms.Form):
+	username = forms.CharField(label='Username')
+	password = forms.CharField(label='Password')
--- index/views.py
+++ index/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from .forms import UserForm
+from .forms import UserRegistrationForm, UserLoginForm
 
 def index(request):
-	form = UserForm()
-	return render(request, 'index.html', {'form':form})
+	l_form = UserLoginForm()
+	r_form = UserRegistrationForm()
+	return render(request, 'index.html', {'l_form':l_form, 'r_form':r_form})
+	
