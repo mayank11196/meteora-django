341eb6bd54bca3309ef6415e78f444d1c1750117
mayank11196 <mayank11195@gmail.com>
2017-04-06 16:21:49 +0530

    Updated index view function

--- index/views.py
+++ index/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from .forms import UserRegistrationForm, UserLoginForm
 
 def index(request):
 	l_form = UserLoginForm()
 	r_form = UserRegistrationForm()
-	return render(request, 'index.html', {'l_form':l_form, 'r_form':r_form})
 	
+	return render(request, 'index.html', {'l_form':l_form, 'r_form':r_form})
