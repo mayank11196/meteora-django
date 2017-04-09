d10bdbe0a4de4ac7fa2b5d1588f5498196c3abd3
mayank11196 <mayank11195@gmail.com>
2017-04-09 14:08:05 +0530

    Modified the index view and template

--- index/views.py
+++ index/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
-from django.urls import reverse
-from .forms import UserRegistrationForm, UserLoginForm
 
 def index(request):
 	
