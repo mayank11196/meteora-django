ca4960f5f7f34e497d6cbce865e337da5b3b5e24
mayank11196 <mayank11195@gmail.com>
2017-04-10 23:01:00 +0530

    Updated login and register templates

--- userlogin/urls.py
+++ userlogin/urls.py
@@ -3,7 +3,7 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [	
-				url(r'^login$', auth_views.login,
+				url(r'^login/$', auth_views.login,
 					{'template_name': '/home/darkknight/Desktop/django_project/meteora-django/userlogin/templates/login.html'}),
-				url(r'^signup$', views.signup),
+				url(r'^signup/$', views.signup),
 			]
