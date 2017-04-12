88dfa9c1fe57c34ff650c85052f49fdf186b2d34
mayank11196 <mayank11195@gmail.com>
2017-04-12 11:59:55 +0530

    Deleted User model from userlogin

--- userlogin/models.py
+++ userlogin/models.py
@@ -4,8 +4,3 @@ from django.db import models
 This file contains the details about the users
 """
 
-class User(models.Model):
-	f_name = models.CharField(max_length=30)
-	l_name = models.CharField(max_length=30)
-	user_name = models.CharField(max_length=20)
-	
