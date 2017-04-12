d00be6b973446b1abd0c3a32a32f6b64623bf74b
mayank11196 <mayank11195@gmail.com>
2017-04-12 11:53:15 +0530

    User authentication fuctioning well updated userlogin files

--- userlogin/forms.py
+++ userlogin/forms.py
@@ -1,7 +1,14 @@
 from django import forms
 
 class UserSignupForm(forms.Form):
-	username = forms.CharField(label="Username")
+	first_name = forms.CharField(max_length=128, label="First Name")
+	last_name = forms.CharField(max_length=128, label="Last Name")
+	username = forms.CharField(max_length=128, label="Username")
 	email = forms.EmailField(label="Email")
-	password = forms.CharField(label="Password")
+	password = forms.CharField(max_length=128, label="Password")
+
+
+class UserLoginForm(forms.Form):
+	username = forms.CharField(max_length=128, label="Username")
+	password = forms.CharField(max_length=128, label="Password")
 
--- userlogin/urls.py
+++ userlogin/urls.py
@@ -1,9 +1,7 @@
 from django.conf.urls import url
-from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [	
-				url(r'^login/$', auth_views.login,
-					{'template_name': '/home/darkknight/Desktop/django_project/meteora-django/userlogin/templates/login.html'}),
+				url(r'^login/$', views.signin),
 				url(r'^signup/$', views.signup),
 			]
