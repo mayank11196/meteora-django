05275fa2033368670e01437d234c8bd56ee4c1c8
mayank11196 <mayank11195@gmail.com>
2017-07-01 00:28:45 +0530

    Edited the mars page

--- mars/api/mars.py
+++ mars/api/mars.py
@@ -1,9 +1,11 @@
 import requests
 
-url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=fhaz&api_key=DEMO_KEY"
+url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=fhaz"
 
 def curiosity():
+	app_token = "DEMO_KEY"
+	payload = {'api_key': app_token}
 
-	response = requests.get(url)
+	response = requests.get(url, params=payload)
 
 	return response.json()
